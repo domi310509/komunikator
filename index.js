1482eedc694a5b4ee536c6c8201b2e2de49c21ae
domi310509 <68813541+domi310509@users.noreply.github.com>
2025-04-24 13:44:16 +0200

    Aktualizacja serwera i strony
    Czas: 20 minut

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
+
+app.use(express.static('public')); // ustaw ze public ma wszystkie pliki dla uzytkownika
 
 app.get('/', (req, res) => {
     res.send('Serwer działa!');
 });
 
+
+// odpal server
 app.listen(PORT, () => {
     console.log(`Serwer nasłuchuje na http://localhost:${PORT}`);
 });
