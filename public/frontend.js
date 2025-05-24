629459544425c7e5cd35979b639785b9b1aa4709
Emix999 <emilianhernes2@gmail.com>
2025-05-24 23:00:31 +0200

    Ekran logowania koniec
    Zrobiłem żeby ekran logowania wyglądał dobrze. Myślę że wygląda to już dobrze, jeśli są jakieś uwagi to proszę do mnie.
    *2 godziny*

--- public/frontend.js
+++ public/frontend.js
@@ -3,39 +3,57 @@ console.log("frontend.js się wczytuje");
 function walidacjaLogowania(){
     let loginU = document.getElementById('logowanieLogin');
     let haslo = document.getElementById('logowanieHaslo');
+    let czyPoprawneDane=true;
     if(loginU.value==''){
-        alert('Musisz podać login');
-        return 0;
+        loginU.style.borderColor='red';
+        if(!loginU.classList.toggle("podajDane"))loginU.classList.toggle("podajDane");
+        loginU.placeholder='Podaj login';
+        czyPoprawneDane=false;
     }
     if(haslo.value==''){
-        alert('Musisz podać hasło');
-        return 0;
+        haslo.style.borderColor='red';
+        if(!haslo.classList.toggle("podajDane"))haslo.classList.toggle("podajDane");
+        haslo.placeholder='Podaj haslo';
+        czyPoprawneDane=false;
     }
+    if(czyPoprawneDane)login(loginU, haslo);
 }
 
 function walidacjaRejestracji(){
     let loginU = document.getElementById('rejestracjaLogin');
     let haslo = document.getElementById('rejestracjaHaslo');
     let haslo2 = document.getElementById('rejestracjaHaslo2');
-    let imie = document.getElementById('rejestracjaImie');
-    let nazwisko = document.getElementById('rejestracjaNazwisko');
-    if(imie.value==''){
-        alert('Musisz podać imie');
-        return 0;
-    }
+    let rozneHasla = document.getElementById('rozneHasla');
+    let czyPoprawneDane=true;
+
     if(loginU.value==''){
-        alert('Musisz podać login');
-        return 0;
+        loginU.style.borderColor='red';
+                if(!loginU.classList.toggle("podajDane"))loginU.classList.toggle("podajDane");
+
+        loginU.placeholder='Podaj login';
+        czyPoprawneDane=false;
     }
     if(haslo.value==''){
-        alert('Musisz podać hasło');
-        return 0;
+        haslo.style.borderColor='red';
+        if(!haslo.classList.toggle("podajDane"))haslo.classList.toggle("podajDane");
+        haslo.placeholder='Podaj haslo';
+        czyPoprawneDane=false;
     }
-
 
     if(haslo.value!=haslo2.value){
-        alert("Podane hasła nie zgadzają się");
-        return 0;
+        haslo2.style.borderColor='red';
+        if(!haslo2.classList.toggle("podajDane"))haslo2.classList.toggle("podajDane");
+        rozneHasla.style.display='block';
+        czyPoprawneDane=false;
     }
-    register(loginU, haslo);
+    if(czyPoprawneDane)register(loginU, haslo);
+}
+function zresetujStyleInputow(input){
+    if(input.style.borderColor=='red')input.style.borderColor='rgb(39, 39, 39)';
+    if(input.classList.toggle("podajDane"))input.classList.toggle("podajDane");
+    if(input.placeholder=='Powtórz hasło')document.getElementById('rozneHasla').style.display='none';
+}
+inputy=document.querySelectorAll('input');
+for(let i of inputy){
+    i.addEventListener('click',()=>zresetujStyleInputow(i));
 }
