3c1c3f425b0bf5e0431999624755844848ed29bc
Oloksos <didyk.aleksander@gmail.com>
2025-05-28 23:07:58 +0200

    Ustawienia i zrozumienie Dominika 4h

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,2 +1,12 @@
 translate();
 
+const ustawienia = document.getElementById("ustawieniaPasekBoczny");
+const poleUstawien = document.getElementById("ustawienia");
+
+ustawienia.addEventListener('click', () => {
+      if (poleUstawien.style.display === 'none') {
+        poleUstawien.style.display = 'flex';
+      } else {
+        poleUstawien.style.display = 'none';
+      }
+    });
