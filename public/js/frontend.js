989f8381a9426cf0cbf986dafac7a0c0251c84de
Werpat7 <niemamnazwy123453@gmail.com>
2025-05-31 21:24:27 +0200

    drobne zmiany
    nie wiem czy to jest dobrze, bo nie mieałem jak sprawdzić

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,2 +1,8 @@
 translate();
 
+document.getElementById("polePisania").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // zapobiega np. wysyłaniu formularza
+    document.getElementById("wyslij").click(); // symuluje kliknięcie przycisku
+  }
+});
