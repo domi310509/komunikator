3c2287e46ef62a0e875f57811ad645d9c44703ae
Emix999 <emilianhernes2@gmail.com>
2025-05-31 21:45:19 +0200

    Update frontend.js

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,8 +1,17 @@
 translate();
 
-document.getElementById("polePisania").addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault(); // zapobiega np. wysyłaniu formularza
-    document.getElementById("wyslij").click(); // symuluje kliknięcie przycisku
-  }
-});
+/*
+Funkcja pobiera kounikat jaki ma wyświetlić i wyświetla go w elemencie o id "blad".
+*/
+function wyswietlanieBledow(tekstBledu){
+    let poleBledu;
+    try{
+        poleBledu= document.getElementById("blad");
+    }
+    catch{
+        return;
+    }
+    poleBledu.style.display = "block";
+    poleBledu.innerText=tekstBledu;
+}
+//setTimeout(()=>wyswietlanieBledow('tesowy blad do testow zobacze czy dziala'),5000);
